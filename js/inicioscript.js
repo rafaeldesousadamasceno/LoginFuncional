d4a48cff5aa0be553a7cd750e90fbad7b80e33d0
Rafael Damasceno <rafaelsd389@gmail.com>
2023-01-23 22:58:30 -0300

    Nova validação para ver se existe TOKEN ou USUÁRIO LOGADO

--- js/inicioscript.js
+++ js/inicioscript.js
@@ -1,4 +1,4 @@
-if(localStorage.getItem('token') == null) {
+if(localStorage.getItem('token') == null || localStorage.getItem('usuarioLogado') == null) {
     alert('Você precisa estar logado para acessar essa página');
     window.location.href = 'https://damascenologin.netlify.app/';
 } else {
