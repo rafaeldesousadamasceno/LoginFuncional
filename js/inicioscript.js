36117dcfd22f6390685acecfa71f0fa586339fcf
Rafael Damasceno <rafaelsd389@gmail.com>
2023-01-23 23:00:14 -0300

    Chama a função SAIR se não tiver TOKEN ou USUÁRIO LOGADO

--- js/inicioscript.js
+++ js/inicioscript.js
@@ -1,6 +1,6 @@
 if(localStorage.getItem('token') == null || localStorage.getItem('usuarioLogado') == null) {
     alert('Você precisa estar logado para acessar essa página');
-    window.location.href = 'https://damascenologin.netlify.app/';
+    sair();
 } else {
     let usuarioLogado = JSON.parse(localStorage.getItem('usuarioLogado'));
     let logado = document.getElementById('logado');
